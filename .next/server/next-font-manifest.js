35820664b009c69103b0607f1ed18b5cd419b3b2
Abbas <abbas.hussain@outlook.com>
2024-11-20 21:55:28 +0400

    removing .next from git

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/abbas/Developer/jawab-home/src/app/layout\":[\"static/media/07a54048a9278940-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oqxZRYMjgDBrnceaY0L+PmBmD4AFF4shiik+r62lKcE=\"\n}"
