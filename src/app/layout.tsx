b751b935b6b3db0b02e2f388f2adb8e42bad4006
Abbas <abbas.hussain@outlook.com>
2024-11-22 23:26:58 +0400

    Adding plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { type Metadata } from 'next'
 import { Outfit } from 'next/font/google'
 import clsx from 'clsx'
-
+import PlausibleProvider from 'next-plausible'
 import '@/styles/tailwind.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import Script from 'next/script'
@@ -27,25 +27,27 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
-      <html
-        lang="en"
-        className={clsx('bg-gray-50 antialiased', outfit.variable)}
-      >
-        <head>
-          <Script
-            src="//cdn.trackdesk.com/tracking.js"
-            strategy="afterInteractive"
-          />
-          <Script id="trackdesk-init" strategy="afterInteractive">
-            {`
+    <PlausibleProvider domain="getjawab.com">
+      <ClerkProvider>
+        <html
+          lang="en"
+          className={clsx('bg-gray-50 antialiased', outfit.variable)}
+        >
+          <head>
+            <Script
+              src="//cdn.trackdesk.com/tracking.js"
+              strategy="afterInteractive"
+            />
+            <Script id="trackdesk-init" strategy="afterInteractive">
+              {`
     (function(t,d,k){(t[k]=t[k]||[]).push(d);t[d]=t[d]||t[k].f||function(){(t[d].q=t[d].q||[]).push(arguments)}})(window,"trackdesk","TrackdeskObject");
     trackdesk('jawab', 'click');
   `}
-          </Script>
-        </head>
-        <body>{children}</body>
-      </html>
-    </ClerkProvider>
+            </Script>
+          </head>
+          <body>{children}</body>
+        </html>
+      </ClerkProvider>
+    </PlausibleProvider>
   )
 }
